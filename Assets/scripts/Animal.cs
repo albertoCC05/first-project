f9d77987eab16c7b15db9d390034098b02641f66
albertoCC05 <alberto.cst05@gmail.com>
2023-10-03 13:05:10 +0200

    empezamos a ver scripts

---
+++ Assets/scripts/Animal.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private int numberLegs = 4;
+   
+   
+}
---
+++ Assets/scripts/MyFirstScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{  
+    // variables practica
+    
+    private int age = 18;
+
+    private float speed = 1.5f;
+
+    private bool isgameover = false;
+
+    private string playername = "Banano_Z";
+}
